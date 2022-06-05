15e957084fdfc1afb154c9d5b07f8c46427ed679
Eyuel Endale <94711621+Eyuel21@users.noreply.github.com>
2022-06-05 07:49:04 +0300

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -17,26 +17,26 @@ for (k = 48; k <= 57; k++)
 for (j = 48; j <= 57; j++)
 {
 if (((k + j) > (c + i) &&  k >= c) || c < k)
-																			{
-																																																putchar(c);
-																																																						putchar(i);
-																																																												putchar(' ');
-																																																																		putchar(k);
-																																																																								putchar(j);
-																																																																													if (c + i + k + j == 227 && c == 57)
-																																																																																			{
-																																																																																										break;
-																																																																																															}
-																																																																																		else
-																																																																																								{
-																																																																																															putchar(',');
-																																																																																																					putchar(' ');
-																																																																																																										}
-																																																																																							}
-																																							}
-																											}
-																	}
-									}
-				putchar('\n');
-					return (0);
+{
+putchar(c);
+putchar(i);
+putchar(' ');
+putchar(k);
+putchar(j);
+if (c + i + k + j == 227 && c == 57)
+{
+break;
+}
+else
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
 }
