1dc55e54652a400668f54929dc571b22ee32f98c
Eyuel21 <eyuelendale7@gmail.com>
2022-06-13 21:58:15 +0300

    5

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
-fndef MAIN_H
+#ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -12,4 +12,3 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
-#endif /* MAIN_H */
