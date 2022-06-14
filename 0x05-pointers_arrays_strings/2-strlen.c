7d2184c04c09a1d8d55b298636b58295ec0b324a
Eyuel21 <eyuelendale7@gmail.com>
2022-06-14 14:18:08 +0300

    5

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
  ** _strlen - returns the length of a string.
  ** @s: character value.
@@ -8,6 +7,8 @@
  **/
 int _strlen(char *s)
 {
-
-return (strlen(s);
+int i;
+for (i = 0; s[i] != 0; i++)
+;
+return (i);
 }
