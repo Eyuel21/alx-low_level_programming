9ae03ff0f31bf0da569f1702bee571afb27507c2
Eyuel21 <eyuelendale7@gmail.com>
2022-07-22 01:59:39 +0300

    yes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,22 @@
-#include "main.h"
+#include "main.h"
 /**
-** binary_to_uint - converts a binary number to an unsigned int.
-** @b: pointer to a string containing a binary number
-**
-** Return: unsigned int with decimal value of binsry number, or 0 if error
-**/
-unsigned int binary_to_uint(const char *b)
+ ** binary_to_uint - converts a binary
+ ** @b: pointer char
+ ** Return: the converted number
+ **/
+unsigned int binary_to_uint(const char *b)
 {
-int i;
-unsigned int num;
-num = 0;
-if (!b)
-return (0);
-for (i = 0; b[i] != '\0'; i++)
+unsigned int azi, vec = 0;
+if (!b)
+return (0);
+for (azi = 0; b[azi] != '\0'; azi++)
 {
-if (b[i] != '0' && b[i] != '1')
-return (0);
+if (b[azi] != '0' && b[azi] != '1')
+return (0);
+if (b[azi] == '1')
+vec = vec * 2 + 1;
+else
+vec = vec * 2 + 0;
 }
-for (i = 0; b[i] != '\0'; i++)
-{
-num <<= 1;
-if (b[i] == '1')
-num += 1;
-}
-return (num);
+return (vec);
 }
