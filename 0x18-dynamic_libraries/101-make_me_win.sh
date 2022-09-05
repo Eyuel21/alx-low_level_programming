2b0a3ec06f5ac4a00ea68dca676f3b2b675e0263
Eyuel21 <eyuelendale7@gmail.com>
2022-09-05 21:08:52 +0300

    0x18-dynamic_libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tnp/ https://raw.github.com/Eyuel21/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
+wget -P /tmp/ https://raw.github.com/Eyuel21/alx-low_level_programming/main/0x18-dynamic_libraries/preload.so
 export LD_PRELOAD=/tmp/preload.so
